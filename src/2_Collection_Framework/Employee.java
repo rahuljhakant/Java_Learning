264fb6d36917523f4ff646bd40bf5a6f6b6562c3
Rahul Kant jha <rahulkantjha@hotmail.com>
2021-12-26 15:01:33 +0530

    refactored all the existing codes

---
+++ src/2_Collection_Framework/Employee.java
@@ -0,0 +1,14 @@
+package Collection_Frameworks_List;
+
+public class Employee {
+
+  String name;
+  int age;
+  String department;
+
+  public Employee(String name, int age, String department) {
+    this.name = name;
+    this.age = age;
+    this.department = department;
+  }
+}
