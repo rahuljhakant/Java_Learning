1524214bc6d2d42edc0a63f2bcbdfa6224bb39ab
Rahul Kant jha <rahulkantjha@hotmail.com>
2021-12-28 10:36:38 +0530

    multidimensional arrays

---
+++ src/main/java/Java_Basics/g_arrays.java
@@ -0,0 +1,10 @@
+package Java_Basics;
+
+public class g_arrays {
+    public static void main(String[] args) {
+        int[][] a = {{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}, {11, 12, 13, 14, 15}};
+        int[][][] b = new int[10][10][10];
+        int[][][][] c = new int[10][10][10][10];
+
+    }
+}
