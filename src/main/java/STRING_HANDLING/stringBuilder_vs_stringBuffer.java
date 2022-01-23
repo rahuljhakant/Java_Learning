d3a5588f62cc8d2c7aa2f7c3ede505a1f7db8000
Rahul Kant jha <rahulkantjha@hotmail.com>
2022-01-23 16:43:59 +0530

    stringBuilder_vs_stringBuffer

--- src/main/java/STRING_HANDLING/stringBuilder_vs_stringBuffer.java
+++ src/main/java/STRING_HANDLING/stringBuilder_vs_stringBuffer.java
@@ -4,7 +4,12 @@ public class stringBuilder_vs_stringBuffer {
 
     /* Difference between string buffer and string builder is,
 
-    String buffer is thread safe, i:e synchronized, whereas String builder is not thread safe i:e not synchronized.
+    1. String buffer is thread safe, i:e synchronized, whereas String builder is not thread safe i:e not synchronized.
 
+    2. String buffer(performance is moderate) is slow due to synchronization and String builder(performance is very high) is fast.
+
+    3. String buffer storage area is in the heap and string builder storage area is the stack.
+
+    4. String buffer consumes more memory and String builder consumes less memory.
     */
 }
