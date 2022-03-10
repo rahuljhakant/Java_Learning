75c3e06f0335a0597596a2a20da3cc6df3cb7696
Rahul Kant jha <rahulkantjha@hotmail.com>
2022-03-10 23:58:34 +0530

    reverse an array and a sentence

---
+++ src/main/java/Coding_Questions/reverse_an_array.java
@@ -0,0 +1,14 @@
+package Coding_Questions;
+
+import java.util.*;
+
+public class reverse_an_array {
+    public static void main(String[] args) {
+        int[] integerArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] reversedArray = new int[integerArray.length];
+        for (int i = integerArray.length - 1; i > 0; i--) {
+            reversedArray[i] = integerArray[i];
+        }
+        System.out.println(Arrays.toString(reversedArray));
+    }
+}
---
+++ src/main/java/Coding_Questions/reverse_word_in.java
@@ -0,0 +1,10 @@
+package Coding_Questions;
+public class reverse_word_in {
+    public static void main(String[] args) {
+        String sentence = "My name is Rahul Kant Jha";
+        String[] words = sentence.split(" ");
+        for (int i = words.length - 1; i >= 0; i--) {
+            System.out.print(words[i]+ " ");
+        }
+    }
+}
