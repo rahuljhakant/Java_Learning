0903c1008ced067256929b3248b44e3a621b391c
Rahul Kant jha <rahulkantjha@hotmail.com>
2022-03-10 23:36:49 +0530

    reverse a string

---
+++ src/main/java/Coding_Questions/String_reverse.java
@@ -0,0 +1,10 @@
+package Coding_Questions;
+public class String_reverse {
+    public static void main(String[] args) {
+        String s = "/.desrever eb ot sdeen hcihw gnirtS eht si sihT";
+        char[] ch = s.toCharArray();
+        for (int i = ch.length - 1; i > 0; i--) {
+            System.out.print(ch[i]);
+        }
+    }
+}
