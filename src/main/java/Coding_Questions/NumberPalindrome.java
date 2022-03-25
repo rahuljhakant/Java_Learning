9a7e3dc969de63dca2ba380abefba77654728202
Rahul Kant jha <rahulkantjha@hotmail.com>
2022-03-25 21:06:04 +0530

    util

--- src/main/java/Coding_Questions/NumberPalindrome.java
+++ src/main/java/Coding_Questions/NumberPalindrome.java
@@ -1,14 +1,15 @@
 package Coding_Questions;
-
+import java.util.logging.Level;
 import java.util.Arrays;
+import utility.*;
 
-public class number_palindrome {
+public class NumberPalindrome implements Util {
     public static void main(String[] args) {
         int a = 1234554321;
         String str = String.valueOf(a);
         char[] chars = str.toCharArray();
         char[] revChar = new char[chars.length];
         System.arraycopy(chars, 0, revChar, 0, chars.length - 1 + 1);
-        Arrays.equals(chars, revChar);
+        logger.log(Level.INFO, String.valueOf(Arrays.equals(chars, revChar)), "Checking if integer number is palindrome or not.");
     }
 }
---
+++ src/main/java/utility/Util.java
@@ -0,0 +1,7 @@
+package utility;
+import java.util.logging.Logger;
+
+public interface Util {
+    Logger logger =
+            Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+}
